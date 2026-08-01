abb747ed829e81d12e43923f604b39fa25fd3131
Jacob Scott <jscott2718@gmail.com>
2026-08-01 11:56:58 +0100

    feat: add scripts/startup.R

---
+++ scripts/startup.R
@@ -0,0 +1,6 @@
+# These options make a pretty big difference to how things are printed in the
+# console, especially if you're a tidyverse user.
+options(cli.default_num_colors = 256L)
+options(cli.dynamic = TRUE)
+options(cli.ansi = TRUE)
+options(cli.hyperlink = TRUE)
