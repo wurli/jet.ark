d0ab0b5f57a3b6e22bc6b5275ab2c04860ab9697
Jacob Scott <jscott2718@gmail.com>
2026-08-18 10:10:06 +0100

    refactor: add rpc_message helper

--- lua/jet/ark/comm.lua
+++ lua/jet/ark/comm.lua
@@ -1,19 +1,25 @@
 local M = {}
 
----@param method string
----@param params any
-M.call_method = function(method, params)
+local rpc_message = function(method, params)
 	return {
 		jsonrpc = "2.0",
 		-- The id field makes this a JSON-RPC message according to Ark:
 		-- https://github.com/posit-dev/positron/issues/7448
 		id = vim.fn.rand(),
-		method = "call_method",
-		params = {
-			method = method,
-			params = params,
-		},
+		method = method,
+		params = params,
 	}
 end
+
+---@param method string
+---@param params any
+M.call_method = function(method, params)
+	return rpc_message("call_method", {
+		method = method,
+		params = params,
+	})
+end
+
+M.show_help_topic = function() end
 
 return M
